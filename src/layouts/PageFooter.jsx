099e7b86df6b3c5c5189e4d6fc27b76175d35ad8
Ehsan Zarei <ehsannzzarei@gmail.com>
2026-07-18 09:42:28 +0330

    Updating the page footer

--- src/layouts/PageFooter.jsx
+++ src/layouts/PageFooter.jsx
@@ -1,8 +1,23 @@
+// Icon
+import { BsGithub } from "react-icons/bs";
+
 export default function PageFooter() {
   return (
     <footer className="print:hidden mx-auto max-w-7xl py-8">
       <div>
-        <p className="text-center text-gray-600 ">Designed By Ehsan Zarei.</p>
+        <p className=" text-stone-600 flex items-center justify-center">
+          Built and designed By
+          <a
+            href="https://github.com/EhsanZareiDev"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mx-2 text-stone-800 font-semibold inline-flex items-center"
+          >
+            <BsGithub className="inline-block text-lg mr-1" />
+            Ehsan Zarei.
+          </a>
+          ❤️
+        </p>
       </div>
     </footer>
   );
