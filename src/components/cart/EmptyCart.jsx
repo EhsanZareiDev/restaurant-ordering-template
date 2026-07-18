81f86c12fe06b432047c2ecf7a8c1c61639dcd06
Ehsan Zarei <ehsannzzarei@gmail.com>
2026-07-18 17:12:08 +0330

    feat: Create EmptyCart for when the shopping cart is empty

---
+++ src/components/cart/EmptyCart.jsx
@@ -0,0 +1,7 @@
+export default function EmptyCart() {
+  return (
+    <div className="mt-40 text-center">
+      <h3 className="text-stone-500 text-xl">Your shopping cart is empty.</h3>
+    </div>
+  );
+}
